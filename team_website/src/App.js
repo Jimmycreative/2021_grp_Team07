c3f090d5b9362b17026c161fee5c7cd931607af3
Jimmycreative <jimmycher222@gmail.com>
2021-10-17 21:28:00 +0800

    team website

---
+++ team_website/src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import TeamDoc from "./TeamDoc"
+import './App.css';
+
+function App() {
+  
+    return (
+      <TeamDoc/>
+    )
+  
+}
+
+export default App;
---
+++ team_website/src/TeamDoc.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+class TeamDoc extends React.Component {
+    render(){
+        return (
+            <div>
+                {/* <!-- Wrapper --> */}
+			<div id="wrapper">
+
+				{/* <!-- Header --> */}
+					<header id="header" class="alt">
+						<span class="logo"><img src="images/logo.svg" alt="" /></span>
+						<h1>Team 202107</h1>
+					</header>
+
+			
+
+				{/* <!-- Main --> */}
+					<div id="main">
+
+						{/* <!-- Introduction --> */}
+							<section id="intro" class="main special">
+								
+								<header class="major">
+									<h2>Member Information</h2>
+								</header>
+							    <p>CHO, In Jae (20212687) CS</p>
+                                <p>FANG, Yichu (20214756) CS</p>
+                                <p>LAU, Yik Lun Yelan (20217531) CS</p>
+                                <p>PARK, So-Young (20121536) CSAI</p>
+                                <p>YANG, Jiun-Chi (20215187) CSAI</p>
+                                <p>WON, Minhyeon (20314810) CSAI</p>
+								
+									
+								
+							</section>
+
+						{/* <!-- First Section --> */}
+							<section id="first" class="main special">
+								<header class="major">
+									<h2>Project Title <br/></h2>
+                                    <h3>Production Job Scheduling Software System</h3>
+								</header>
+								
+							</section>
+
+						{/* <!-- Second Section --> */}
+							<section id="second" class="main special">
+								<header class="major">
+									<h2>Supervisor Name</h2>
+									<h3>Ruibin Bai</h3>
+								</header>
+								
+							</section>
+
+						{/* <!-- Get Started --> */}
+							<section id="cta" class="main special">
+								<header class="major">
+									<h2>Other Relevant Information</h2>
+									
+								</header>
+								<footer class="major">
+									<ul class="actions special">
+										<li><a href="https://developers.google.com/optimization/scheduling/job_shop" class="button primary">Learn More</a></li>
+									</ul>
+								</footer>
+							</section>
+
+					</div>
+
+				{/* <!-- Footer --> */}
+					<footer id="footer">
+						<section>
+							<h2>Citation</h2>
+							<p>(HTML5 UP, 2021)</p>
+							<ul class="actions">
+								<li><a href="https://html5up.net/stellar" class="button">Learn More</a></li>
+							</ul>
+						</section>
+						
+						<p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+					</footer>
+
+			</div>
+
+		{/* <!-- Scripts --> */}
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+    
+            </div>
+        );
+    }
+    
+}
+
+export default TeamDoc;
