a88b731ca85f4d969ef759cbc3f0643f711c275a
Jimmycreative <jimmycher222@gmail.com>
2022-01-04 21:42:29 +0800

    Login System
    Email has to be valid
    Password has to be at least 8 characters

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ValidationLog from "./ValidationLog"
+import Main from "./Main"
+import  {BrowserRouter as Router,Route,Switch,Redirect}  from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
 
-export default App;
+  return (
+    <Router>
+      <div className="App">
+
+        <Switch>
+          <Route exact path="/" component={ValidationLog}></Route>
+          <Route path="/main" component={Main}></Route>
+        </Switch>
+          
+      </div>
+    </Router>
+  )
+  
+}
+export default App
