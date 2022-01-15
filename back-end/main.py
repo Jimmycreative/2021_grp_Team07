3c74340e2b59f95c95cf61770a64ea3fd457ba69
LAU <scyyl18@nottingham.edu.cn>
2022-01-15 15:22:04 +0800

    added function to put token into database

--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,7 @@
 # You should not import any additional modules
 # You can, however, import additional functionalities 
 # from the flask and sqlite3 modules
+import json
 from flask import Flask, render_template, request, redirect, session, url_for
 #from flask_session import Session
 import mysql.connector
@@ -22,6 +23,7 @@ database = mysql.connector.connect(
 #login main
 @app.route('/')
 def index():
+    tokengen()
     return render_template('index.html')
 
 @app.route('/', methods = ['POST'])
@@ -90,8 +92,6 @@ def registration():
         return render_template("token/index.html", msg=err, error=1)
 
 
-
-
 @app.route('/registration/form', methods = ['GET'])
 def registrationform():
     if request.args.get('token', None) is None:
@@ -103,8 +103,8 @@ def registrationform():
 def registrationpost():
     if request.method != 'POST':
         return render_template('registration/index.html')
-    # Check if "username" and "password" POST requests exist (user submitted form)
     
+    # Check if "username" and "password" POST requests exist (user submitted form)
     if 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
@@ -122,17 +122,18 @@ def registrationpost():
 
 
     #check if username already exists
+    #this is a failsafe, please do this in frontend first
     if checkusername(username):
         err = "Username already exists"
         return render_template('registration/index.html', msg=err, error=1, token=usertoken)
 
     #else check token in db
-
-    uses = checktoken(usertoken)
-    cur = database.cursor()
+    tokendata = checktoken(usertoken)
+    uses = tokendata['uses']
     #successful
-    if uses:
-        cur.execute("UPDATE tokens SET uses= (%s - 1) WHERE token = %s;", (uses, usertoken,))
+    if tokendata:
+        cur = database.cursor()
+        cur.execute("UPDATE token SET uses= (%s - 1) WHERE token = %s;", (uses, usertoken,))
         database.commit()
         '''
         cur.execute("""
@@ -160,8 +161,8 @@ def registrationpost():
 ##PLEASE VERIFY WHETHER USERNAME IS VALID BEFORE USING THIS CODE!
 
 #username requriements before feeding to this program:
-# 3-16 characters, first must be A-Za-z, last must be A-Za-z0-9, middle [A-Za-z0-9_] 
-# regex is below
+# 4-16 characters, first must be A-Za-z, last must be A-Za-z0-9, middle [A-Za-z0-9_] 
+# regex to check is below
 # ^[A-Za-z][\w]{2,14}[A-Za-z0-9]$
 
 def checkusername(username):
@@ -179,30 +180,79 @@ def checkusername(username):
     else:
         return False
 
+
 ##for checking token exists
-def checktoken(usertoken):
+def checktoken(token):
     cur = database.cursor(dictionary=True)
     cur.execute("""
-    SELECT uses
-    FROM tokens
+    SELECT uses, rank
+    FROM token
     WHERE token = %s
     AND disabled = 0
     AND (dateexpire IS NULL OR dateexpire > CURRENT_TIMESTAMP()) 
     AND uses <> 0;
-    """, (usertoken,))
+    """, (token,))
 
     tokendata = cur.fetchone()
 
-    if tokendata:
-        return tokendata['uses']
+    return tokendata
+
+#for putting into database
+## REQUIREMENTS
+## dateexpire must be in format "YYYY-MM-DD hh:mm:ss", if set to "never expire", put None
+## uses must be integer, if set to unlimited uses, put -1 or None
+
+## rank guide
+### 0 = user
+### 1 = planner
+### 2 = manager
+def puttoken(token, dateexpire, rank, uses):
+    cur = database.cursor()
+    if token is None:
+        token = tokengen()
+    if rank is None:
+        rank = 0
+    if uses is None:
+        uses = -1
+
+    if dateexpire is None:
+        cur.execute("""
+        INSERT INTO token (token, dateexpire, rank, uses)
+        VALUES (%s, NULL, %s, %s);
+        """, (token, rank, uses))
     else:
-        return False
+        cur.execute("""
+        INSERT INTO token (token, dateexpire, rank, uses)
+        VALUES (%s, %s, %s, %s);
+        """, (token, dateexpire, rank, uses))
+
+    database.commit()
+    
+    
 
 # For generating token
-'''
 def tokengen():
-    token = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(128))
-'''
+    token = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(8))
+    flag=False
+
+    while checktoken(token):
+        if flag:
+            purgetoken()
+        token = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(8))
+        flag=True
+
+    return token
+
+#for purging invalid tokens
+def purgetoken():
+    cur = database.cursor()
+    cur.execute("""
+    DELETE FROM token
+    WHERE disabled = 1
+    OR dateexpire < CURRENT_TIMESTAMP()
+    OR uses = 0;
+    """)
+    database.commit()
 
 
 @app.route('/home')
