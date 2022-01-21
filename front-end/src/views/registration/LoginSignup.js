98527e4a5a9a15d4e0bfa2e0d99b77f95cefe6fc
fyyc <854490190@qq.com>
2022-01-21 15:36:37 +0800

    fix: registration display problem

--- front-end/src/views/registration/LoginSignup.js
+++ front-end/src/views/registration/LoginSignup.js
@@ -39,6 +39,128 @@ function LoginSignup() {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    
+    const [displayname, setDisplayname] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [token, setToken] = useState('');
+
+    const [usernameError, setUsernameError] = useState('');
+    const [displaynameError, setDisplaynameError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [password2Error, setPassword2Error] = useState('');
+    const [tokenError, setTokenError] = useState('');
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const submitRegister = e => {
+        e.preventDefault();
+
+        let isValid = true;
+        let fieldsList = ["username", "displayname", "password", "password2"];
+
+        for (let field in fieldsList) {
+
+            isValid = isField(fieldsList[field]) && isValid;
+        }
+
+        if (isValid) {
+            setIsSubmitted(true);
+        } else {
+            setIsSubmitted(false);
+        }
+
+        return isSubmitted;
+    };
+
+
+    const isField = (field) => {
+
+        let isValid = false;
+
+        switch (field) {
+            case "username":
+                isValid = usernameValidation();
+                break;
+            case "displayname":
+                isValid = displaynameValidation();
+                break;
+            case "password":
+                isValid = passwordValidation();
+                break;
+            case "password2":
+                isValid = matchPassword();
+                break;
+            default:
+        }
+
+        return isValid;
+    }
+
+
+
+    const usernameValidation = () => {
+
+        setUsername(username.toLowerCase());
+
+        const usernameRegex = /^[A-Za-z][\w]{2,14}[A-Za-z0-9]$/;
+        if (username.trim() === "") {
+            setUsernameError('Username required');
+            return false
+        } else if (!usernameRegex.test(username)) {
+            setUsernameError("Username must be 4-16 characters with no space and special character");
+            return false
+        }
+
+        setUsernameError('');
+        return true;
+    }
+
+
+    const displaynameValidation = () => {
+
+        const replaceSpaces = displayname.replace(/\s+(?=\s)/g, '').trim();
+        setDisplayname(replaceSpaces);
+
+        if (displayname === "") {
+            setDisplayname(username);
+            return true;
+        } else if (displayname.length > 64) {
+            setDisplayname(displayname.substring(0, 64) && replaceSpaces);
+        }
+
+        setDisplaynameError('');
+        return true;
+    }
+
+
+    const passwordValidation = () => {
+
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,30}$/;
+        if (password.trim() === "") {
+            setPasswordError("Password required");
+            return false;
+        } else if (!passwordRegex.test(password)) {
+            setPasswordError("Password needs to be 8-30 characters including at least 1 digit, 1 upper and 1 lowercase");
+            return false;
+        }
+
+        setPasswordError("");
+        return true;
+    }
+
+    const matchPassword = () => {
+
+        if (password2.trim() === "") {
+            setPassword2Error("Password required");
+            return false;
+        } else if (password !== password2) {
+            setPassword2Error("Passwords do not match");
+            return false
+        }
+
+        setPassword2Error("");
+        return true;
+    }
 
     const submitLogin = (e) => {
         e.preventDefault()
@@ -85,13 +207,91 @@ function LoginSignup() {
                         </form>
                     </div>}
                 
-                {!click && <form className="input-group">
-                {/* <Registration/> */}
-                </form>}
-            </div>  
-        </div>
-        </>
-    )
-}
+                {!click &&
+                    <div className="registration">
+                        <br />
+                        <br />
+                        {isSubmitted?
+                            <div style={{ textAlign: "center" }} className="complete">
+                                <h1>Complete registration!</h1>
+                                <div>Username is {username}</div>
+                                <div>Displayname is {displayname}</div>
+                            </div>
+                            :
+                            <div className="registration-form" style={{ textAlign: "center" }}>
+                                <div className="container">
+                                <form onSubmit={submitRegister} >
+                                    <div style={{ marginTop: "80px" }}>
+                                    <h5> CREATE ACCOUNT</h5>
+                                    <h6>Enter every field to join us</h6>
+                                    </div>
+                                
+                                <div className="detail-container">
+                                    <div className="input-box">
+                                        <input
+                                            type="text"
+                                            placeholder="Token"
+                                            name="token"
+                                            value={token}
+                                            onChange={(e) => setToken(e.target.value)}
+                                        />
+                                        {tokenError && (<div className="error">{tokenError}</div>)}
+                                    </div>
+                                    <div className={!displaynameError ? "input-box": "no-margin"}>
+                                        <input
+                                            type="text"
+                                            placeholder="Username"
+                                            name="username"
+                                            value={username}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                        />
+                                        {usernameError && (<div className="error">{usernameError}</div>)}
+                                    </div>
+
+                                    <div className={!displaynameError ? "input-box":"no-margin"}>
+                                        <input
+                                            type="text"
+                                            placeholder="Displayname"
+                                            name="displayname"
+                                            value={displayname}
+                                            onChange={(e) => setDisplayname(e.target.value)}
+                                        />
+                                        {displaynameError && (<div className="error">{displaynameError}</div>)}
+                                    </div>
+
+                                    <div className={!displaynameError ? "input-box":"no-margin"}>
+                                        <input
+                                            type="password"
+                                            placeholder="Password"
+                                            name="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        {passwordError && (<div className="error">{passwordError}</div>)}
+                                    </div>
+                                    
+                                    <div className={!displaynameError ? "input-box":"no-margin"}>
+                                        <input
+                                            type="password"
+                                            placeholder="Confirm password"
+                                            name="password2"
+                                            value={password2}
+                                            onChange={(e) => setPassword2(e.target.value)}
+                                        />
+                                        {password2Error && (<div className="error">{password2Error}</div>)}
+                                    </div>
+                                </div>
+                                
+                                    <button className="submit-btn" type="submit" >Register</button>
+                                </form>
+                                </div>
+                            </div>}
+                        </div>}
+                    </div>
+                </div>
+            </>
+        )
+    }
+
 
 export default LoginSignup; 
