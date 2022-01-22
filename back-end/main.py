cfc1f4b9bd615172a3e006dcb66a9506c7d8b155
Jimmycreative <jimmycher222@gmail.com>
2022-01-22 17:13:31 +0800

    Covert the data format receiving from the algorithm Try catch statements are added in the backend but just those api is done in the front end and algorithm.

--- back-end/main.py
+++ back-end/main.py
@@ -36,8 +36,8 @@ CORS(app, supports_credentials=True)
 database = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
-  password="",
-  database="grp"
+  password="12345678",
+  database="try"
 )
 login_info = {
     "code": -1,
@@ -48,6 +48,194 @@ algorithm_result = {}
 
 
 #Below three functions are helper function
+def handle_result(result):
+    res = []
+    dic = {}
+    i = 0
+    
+    while True:
+        if i==len(result):
+            break
+        elif result[i]=="{":
+            key = ""
+            value = ""
+            i+=1
+            while True:
+                if (result[i]=="," and result[i-1]=="}") or (result[i]=="}" and result[i-1]=="]") :
+                    break
+                elif result[i]=="," or result[i]=="}":
+                    i+=1
+                elif result[i]=="'":
+                    i+=1
+                    while True:
+                        if result[i]=="'":
+                            i+=1
+                            break
+                        else:
+                            key+=result[i]
+                            i+=1
+                elif result[i]==":":
+                    if key=="start":
+                        i+=1
+                        while True:
+                            if result[i]=="}" or result[i]==",":
+                                dic[key] = int(value)
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            else:
+                                value+=result[i]
+                                i+=1
+                    elif key=="name":
+                        i+=2 # start from character not '
+                        while True:
+                            if result[i]=="}" or result[i]=="'":
+                                if result[i]=="}":
+                                    res.append(dic)
+                                    dic = {}
+                                    
+                                dic[key] = value
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            else:
+                                value+=result[i] #if for '
+                                i+=1
+                    elif key=="progress":
+                        i+=1
+                        while True:
+                            if result[i]=="}" or result[i]==",":
+                                if result[i]=="}":
+                                    res.append(dic)
+                                    dic = {}
+                                dic[key] = int(value)
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            else:
+                                value+=result[i]
+                                i+=1
+                    elif key=="end":
+                        i+=1
+                        while True:
+                            if result[i]=="}" or result[i]==",":
+                                if result[i]=="}":
+                                    res.append(dic)
+                                    dic = {}
+                                dic[key] = int(value)
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            else:
+                                value+=result[i]
+                                i+=1
+                    elif key=="id":
+                        i+=2 # start from character not '
+                        while True:
+                            if result[i]=="}" or result[i]=="'":
+                                if result[i]=="}":
+                                    res.append(dic)
+                                    dic = {}
+                                dic[key] = value
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            else:
+                                value+=result[i]
+                                i+=1
+                    elif key=="type":
+                        i+=2 # start from character not '
+                        while True:
+                            if result[i]==",":
+                                dic[key] = value
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            elif result[i]=="}":
+                                
+                                dic[key] = value
+                                res.append(dic)
+                                dic = {}
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            elif result[i]!="'":
+                                value+=result[i]
+                            i+=1
+                    elif key=="hideChildren":
+                        i+=1 # start from character not '
+                        while True:
+                            if result[i]=="}" or result[i]==",":
+                            
+                            
+
+                                if value == "false": #false}
+                                    dic[key] = False
+                                elif value == "true":
+                                    dic[key] = True
+
+                                if result[i]=="}":
+                                    res.append(dic)
+                                    dic = {}
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            
+                            else:
+                                value+=result[i]
+                                i+=1
+                    elif key=="project":
+                        i+=2 # start from character not '
+                        while True:
+                            if result[i]=="}" or result[i]=="'":
+                                dic[key] = value
+                                if result[i]=="}":
+                                    res.append(dic)
+                                    dic = {}
+                                
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            else:
+                                value+=result[i]
+                                i+=1
+                    elif key=="dependencies":
+                        i+=2 
+                        temp_ls = [] 
+                        while True:
+                            
+                            if result[i]=="]":
+                                dic[key] = temp_ls
+                                res.append(dic)
+                                dic = {}
+                                key = ""
+                                value = ""
+                                i+=1
+                                break
+                            elif result[i]=="'":
+                                temp_str = ""
+                                i+=1
+                                while True:
+                                    if result[i]=="'":
+                                        temp_ls.append(temp_str)
+                                        temp_str = ""
+                                        i+=1
+                                        break
+                                    else:
+                                        temp_str+=result[i]
+                                        i+=1
+                            
+        i+=1
+    return res
 # For generating token
 def tokengen():
     purgetoken()
@@ -169,6 +357,8 @@ def post_uid():
         algorithm_result = json.loads(r.text)
         return algorithm_result
 
+
+
 @app.route('/getAllSchedules', methods = ['GET'])
 # 1. try catch
 # 2. format
@@ -177,26 +367,37 @@ def getAllSchedule():
     res_list=[]
     cur = database.cursor(dictionary=True)
     # TODO exclude script
-    cur.execute("SELECT * FROM schedule ORDER BY startdate DESC")
+    try:
+        cur.execute("SELECT * FROM schedule ORDER BY startdate DESC")
+    except Exception as e:
+        #print(e)
+        return jsonify({"code":-2, "data":{},"message":e})
+    else:
+        for schedule in cur:
+            res_str=json.dumps(schedule, cls=MyEncoder)
+            #res_str=res_str.replace("'", '"')
+            #res_str=res_str.replace("\\", '')
+            
+            res_json=json.loads(res_str)
+            modify_result = handle_result(res_json["result"])
+            res_json["result"] = modify_result
+            #print(res_json["result"])
+            print("--------------------------")
+            res_list.append(res_json)    
+    finally:
+        cur.close()
     #data = request.json
     # if data:
     #     #skip_schedule = data["skipschedule"]
     #     cur = database.cursor(dictionary=True)
     #     cur.execute("SELECT * FROM schedule ORDER BY startdate DESC")
         # cur.execute("SELECT * FROM schedule ORDER BY startdate DESC LIMIT (%d,%d);",(skip_schedule,skip_schedule+20))
-    for schedule in cur:
-        res_str=json.dumps(schedule, cls=MyEncoder)
-        res_str=res_str.replace("'", '"')
-        print(res_str)
-        res_json=json.loads(res_str)
-        res_list.append(res_json)    
-    cur.close()
     
     mydict={}
     mydict['result']=res_list
     if res_list:
         my_json=jsonify({"code":1, "data":mydict,"message":"Successfully get the schedules!"})
-        print(my_json)
+        #print(my_json)
         return my_json
     else:
         return jsonify({"code":-1, "data":{},"message":"No schedules!"})
@@ -219,7 +420,7 @@ def save_schedule():
         description = data["data"]["description"]
         uuid = data["data"]["uuid"]
         cur.execute("INSERT INTO schedule (name, uid, script, timelength, result, status, errlog, description, uuid) VALUES ( %s, %d, %s, %d, %s, %d, %s, %s, %s);", (name, uid, script, timelength, result, status, errlog, description, uuid))
-        #INSERT INTO schedule (name, uid, script, timelength, result, status, errlog, description, uuid) VALUES ('schedule 1',1,'i am handsome',3,'[{"name":"Maachine_0","tasklist":[{"task_name":"task_0","to_time":3,"from_time":0}]},{"name":"Maachine_1","tasklist":[{"task_name":"task_0","to_time":1,"from_time":0}]}]', -1, "none",'good schedule','8jug7g7g')
+        #INSERT INTO schedule (name, uid, script, timelength, result, status, errlog, description, uuid) VALUES ('schedule 1',1,'i am handsome',3,"[{'start':0,'name':'Maachine 0','progress':0,'end':5,'id':'Machine 0','type':'project','hideChildren':false},{'start':0,'name':'job_0 task_0','progress':0,'project':'Machine 0','end':3,'id':'job_0|task_0','type':'task'},{'start':3,'name':'job_1 task_0','progress':0,'project':'Machine 0','end':5,'id':'job_1|task_0','type':'task'},{'start':0,'name':'Maachine 1','progress':0,'end':10,'id':'Machine 1','type':'project','hideChildren':false},{'start':0,'name':'job_2 task_0','progress':0,'project':'Machine 1','end':4,'id':'job_2|task_0','type':'task'},{'start':4,'name':'job_0 task_1','progress':0,'project':'Machine 1','end':6,'id':'job_0|task_1','type':'task','dependencies':['job_0|task_0']},{'start':6,'name':'job_1 task_2','progress':0,'project':'Machine 1','end':10,'id':'job_1|task_2','type':'task','dependencies':['job_1|task_1']},{'start':4,'name':'Maachine 2','progress':0,'end':9,'id':'Machine 2','type':'project','hideChildren':false},{'start':4,'name':'job_2 task_1','progress':0,'project':'Machine 2','end':7,'id':'job_2|task_1','type':'task','dependencies':['job_2|task_0']},{'start':7,'name':'job_0 task_2','progress':0,'project':'Machine 2','end':9,'id':'job_0|task_2','type':'task','dependencies':['job_0|task_1']},{'start':5,'name':'Maachine 12','progress':0,'end':6,'id':'Machine 12','type':'project','hideChildren':false},{'start':5,'name':'job_1 task_1','progress':0,'project':'Machine 12','end':6,'id':'job_1|task_1','type':'task','dependencies':['job_1|task_0']}]", -1, "none",'good schedule','8jug7g7g');
         database.commit()
 
 
@@ -227,17 +428,21 @@ def save_schedule():
 def test():
    
         data = request.json
-        if data:
-            print(data)
-            username = data['username']
-            password = data['password']
-            cur = database.cursor(dictionary=True)
-            cur.execute("SELECT username, password FROM user WHERE username = %s AND password = %s;", (username, password))
-            #cur.execute("SELECT uid, displayname, rank, disabled FROM user WHERE username = %s AND password = %s;", (username, password,))
-            #cur.execute("SELECT uid, displayname, rank, disabled FROM user WHERE username = %s AND password = AES_ENCRYPT(%s, UNHEX(SHA2('', )));", (username, password,))
-            
-        account = cur.fetchone()
-        database.commit()
+        try:
+            if data:
+                print(data)
+                username = data['username']
+                password = data['password']
+                cur = database.cursor(dictionary=True)
+                cur.execute("SELECT username, password FROM user WHERE username = %s AND password = %s;", (username, password))
+                #cur.execute("SELECT uid, displayname, rank, disabled FROM user WHERE username = %s AND password = %s;", (username, password,))
+                #cur.execute("SELECT uid, displayname, rank, disabled FROM user WHERE username = %s AND password = AES_ENCRYPT(%s, UNHEX(SHA2('', )));", (username, password,))
+        except Exception as e:
+            return jsonify({"code":-2, "data":{},"message":e})
+        finally:
+            account = cur.fetchone()
+            cur.close()
+        
         if account:
             modify_info(1,"Login successfully!")
         else:   
@@ -305,6 +510,7 @@ def genToken():
 @app.route("/")
 def hello_world():
     # post_script() #right no it's used to test the connectivity between backend and the algorithm
+    
     return "hello world"
 
 
