ee985488ec89ecf1bb93af03e71f59234fc8ac86
scyyl18 <92621288+scyyl18@users.noreply.github.com>
2022-03-28 19:41:39 +0800

    Update testSendAssignment.py

--- back-end/testSendAssignment.py
+++ back-end/testSendAssignment.py
@@ -44,14 +44,21 @@ class testSendAssignment(unittest.TestCase):
         response = app.test_client().post('/sendAssignment', data=json.dumps(
             dict(planner="exampleplanner", title="nelrnf234r5", description="nci34u89d")),
             content_type='application/json')
-
+        
+        #get return json
         json_data = response.data
+        #convert json into dictionary
         json_dict = json.loads(json_data)
         
-        #check if code is in json
+        #check if code is in dictionary
         self.assertIn('code', json_dict, 'Wrong format')
+        #check if 'message' is in dictonary
+        self.assertIn('message', json_dict, 'Wrong format')
+        
         #check if code = 1
         self.assertEqual(json_dict['code'], 1, 'Wrong code')
+        #check if message is correct
+        self.assertEqual(json_dict['message'], "success", 'Wrong code')
 
 # ===========================================================================================================================
 
