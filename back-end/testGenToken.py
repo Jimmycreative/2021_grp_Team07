472b86f7ea9d2e560cfbb81dcf14c7281c298f6f
JayTef <62833626+JayTef@users.noreply.github.com>
2022-03-28 20:29:04 +0800

    Update testGenToken.py

--- back-end/testGenToken.py
+++ back-end/testGenToken.py
@@ -31,13 +31,19 @@ class testGenToken(unittest.TestCase):
         
         #check if 'code' exists in return json
         self.assertIn('code', json_dict, 'Wrong format')
+        self.assertIn('message', json_dict, 'Wrong format') 
+
         #check if 'code = 1
         self.assertEqual(json_dict['code'], 1, 'Wrong code')
+        self.assertEqual(json_dict['message'], "Successfully generating token!", 'Wrong message')
+
         
         #check if data (token) exists in return json
         self.assertIn('data', json_dict, 'Wrong format! data does not exist')
         #check if data (token) is empty
         self.assertTrue(json_dict['data'], "Data does not exist")
+        
+        
         #check if data (token) has invalid token characters
         self.assertRegex(json_dict['data'],
                          "^[A-Za-z0-9]{8}$", "Invalid token format")
