156a2d237c98769e6eb56006e4db257fca2ec373
fyyc <854490190@qq.com>
2022-03-31 22:57:06 +0800

    script tests deleted

--- script/magicProject/src/main/java/com/grp/controller/APIController.java
+++ script/magicProject/src/main/java/com/grp/controller/APIController.java
@@ -121,9 +121,9 @@ public class APIController {
      */
     private void getPyPath() {
         String curPath=System.getProperty("user.dir");
-        //TODO
-        //curPath=curPath.replace("magicProject", "algorithm\\");
-        curPath=curPath.replace("magicProject", "algorithm/");
+        //TODO Path
+        curPath=curPath.replace("magicProject", "algorithm\\");
+        //curPath=curPath.replace("magicProject", "algorithm/");
         exePath=curPath;
     }
 
--- script/magicProject/src/main/java/com/grp/service/ModelService.java
+++ script/magicProject/src/main/java/com/grp/service/ModelService.java
@@ -217,11 +217,11 @@ public class ModelService {
      */
     private void getPyPath(ServiceVariable serviceVariable) {
         String curPath = System.getProperty("user.dir");
-        //TODO
-        //curPath=curPath.replace("magicProject", "algorithm\\");
-        //serviceVariable.setPath(curPath+"pymodel\\");
-        curPath = curPath.replace("magicProject", "algorithm/");
-        serviceVariable.setPath(curPath + "pymodel/");
+        //TODO Path
+        curPath=curPath.replace("magicProject", "algorithm\\");
+        serviceVariable.setPath(curPath+"pymodel\\");
+//        curPath = curPath.replace("magicProject", "algorithm/");
+//        serviceVariable.setPath(curPath + "pymodel/");
         serviceVariable.setExePath(curPath);
     }
 
@@ -387,15 +387,15 @@ public class ModelService {
     private void executeCode(ServiceVariable serviceVariable) throws Exception {
         int flag=0;
         try {
-            //TODO
-            //String pyFile="python "+serviceVariable.getExePath()+serviceVariable.getUuid()+".py";
-            //Process proc = Runtime.getRuntime().exec(pyFile);// 执行py文件
+            //TODO Path
+            String pyFile="python "+serviceVariable.getExePath()+serviceVariable.getUuid()+".py";
+            Process proc = Runtime.getRuntime().exec(pyFile);// 执行py文件
 
-            String pyFile=serviceVariable.getExePath()+serviceVariable.getUuid()+".py";
-            //String[] cmd = {"/Users/jiun-chiyang/opt/anaconda3/bin/python",pyFile};
-            String[] cmd = {"/usr/bin/python3",pyFile};
+//            String pyFile=serviceVariable.getExePath()+serviceVariable.getUuid()+".py";
+//            //String[] cmd = {"/Users/jiun-chiyang/opt/anaconda3/bin/python",pyFile};
+//            String[] cmd = {"/usr/bin/python3",pyFile};
 
-            Process proc = Runtime.getRuntime().exec(cmd);// 执行py文件
+            //Process proc = Runtime.getRuntime().exec(cmd);// 执行py文件
 
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line;
