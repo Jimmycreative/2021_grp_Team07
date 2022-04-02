d6669c414d97cc38599b864bb6d805efd59e5703
scyyl18 <92621288+scyyl18@users.noreply.github.com>
2022-04-02 09:44:29 +0800

    Update py-dependancies.bat

--- py-dependancies.bat
+++ py-dependancies.bat
@@ -1,5 +1,8 @@
 python -m ensurepip
 pip install flask
-pip install mysql-connector-python
 pip install flask-session
+pip install flask-cors
+pip install mysql-connector-python
 pip install --upgrade --user ortools
+pip install requests
+pip install SQLAlchemy
