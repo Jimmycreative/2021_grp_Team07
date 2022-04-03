cf3bd7f61c6e37b106b6bf282c821213210a712e
scyyl18 <92621288+scyyl18@users.noreply.github.com>
2022-04-03 11:02:17 +0800

    Update grp_structure.sql

--- back-end/grp_structure.sql
+++ back-end/grp_structure.sql
@@ -88,6 +88,17 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
+-- Table structure for table `magic_api_file`
+--
+
+CREATE TABLE `magic_api_file` (
+`id` bigint(255) NOT NULL AUTO_INCREMENT,
+`file_path` varchar(255) DEFAULT NULL,
+`file_content` text,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+--
 -- Indexes for dumped tables
 --
 
